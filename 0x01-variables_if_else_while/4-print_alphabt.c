2b62b5c6e0f1ab13f2db816f610dd3e681fa6954
Dramani-devfullstack <faizouoroudramani@gmail.com>
2023-02-20 08:32:02 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	char c;
 
-	for (c = "a"; c <= "z"; c++)
-		if (c != "q" && c != "e")
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'q' && c != 'e')
 			putchar(c);
 
 	putchar('\n');

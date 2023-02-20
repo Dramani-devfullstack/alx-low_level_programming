ba37956c3213cb15b023df62a383271228075b04
Dramani-devfullstack <faizouoroudramani@gmail.com>
2023-02-20 09:01:04 -0800

    TAsk 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}

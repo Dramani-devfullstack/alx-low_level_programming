26be69016aa2bdd922ee113c5449f89cd7fb46fe
Dramani-devfullstack <faizouoroudramani@gmail.com>
2023-02-20 08:50:29 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%i", a);
+	}
+		printf("\n");
+		return (0);
+}
